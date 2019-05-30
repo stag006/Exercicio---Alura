44e86f304d0763e77ddb0e6a6de37ace00501da6
Arthur Teixeira <stag006@benner.com.br>
2019-05-29 21:02:16 -0300

    Trabalhando com classe funcionario no projeto 3-CSharp

---
+++ ByteBank/ByteBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank.Funcionarios
+{
+    public class Funcionario
+    {
+        // 0 - funcionario
+        // 1 - Diretor
+        // 2 - Designer
+        // N - ...
+        private int _tipo;
+
+        public string Nome { get; set; }
+        public  string CPF { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int tipo)
+        {
+            _tipo = tipo;
+        }
+
+
+        public double GetBonificao()
+        {
+            if (_tipo == 1)
+            return Salario * 10;
+
+            return Salario * 0.10;
+        }
+
+    }
+}
