04459c28225398e73f645eb854c19d10016e4c28
Arthur Teixeira <stag006@benner.com.br>
2019-05-29 22:21:22 -0300

    Terminando a aula 1 do curso 3CSharp

--- ByteBank/ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/ByteBank/Funcionarios/Diretor.cs
@@ -6,16 +6,15 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Funcionario
+    public class Diretor
     {
-
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
 
         public double GetBonificacao()
         {
-            return Salario * 0.10;
+            return Salario;
         }
     }
 }
--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -12,16 +12,42 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario(1);
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
+            Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "546.879.157-20";
             carlos.Salario = 2000;
 
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "132.465.798-45";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+
+
             Console.WriteLine(carlos.Nome);
-            Console.WriteLine(carlos.Salario);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Console.WriteLine();
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.Salario);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTtotalBonificacao());
 
             Console.ReadLine();
         }
     }
 }
+/*  Podemos criar diretórios na raiz de nosso projeto para organizar melhor o código!
+    O recurso de possíveis soluções do Visual Studio, acessível pelo atalho CTRL+PONTO;
+    Podemos ter vários métodos com o mesmo nome, desde que possuam lista de argumentos diferentes! Estas são as sobrecargas.
+ * 
+ */
